d7eafed14219e7000f0988646a626a122d60de3f
caloulacerda <felipe_calou@hotmail.com>
2024-09-06 11:56:09 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Pikachu",
+        descricao: "Pequeno, amarelo e com duas listras vermelhas nas bochechas, Pikachu é do tipo Elétrico. Suas orelhas pontudas e rabo em forma de raio são suas características mais marcantes.",
+        link: "https://www.pokemon.com/br/pokedex/pikachu",
+        tags: "amarelo rato choque"
+    },
+    {
+        titulo: "Gengar",
+        descricao: "Gengar é um Pokémon do tipo Fantasma/Veneno conhecido por suas travessuras e sua habilidade de se tornar invisível. Sua aparência peculiar e seus olhos brilhantes o tornam um Pokémon memorável.",
+        link: "https://www.pokemon.com/br/pokedex/gengar",
+        tags: "fantasma veneno sorriso"
+    },
+    {
+        titulo: "Chicorita",
+        descricao: "Um Pokémon gentil com uma folha verde na cabeça. Ele pode sentir mudanças no clima.",
+        link: "https://www.pokemon.com/br/pokedex/chikorita",
+        tags: "folha planta verde"
+    }
+];
